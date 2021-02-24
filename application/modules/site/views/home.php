c3901e659d0e01e73e1319835996cfad4077fb8d
mobarokhossen <mobarokaiub@gmail.com>
2021-02-24 12:24:47 +0600

    Documentation

--- application/modules/site/views/home.php
+++ application/modules/site/views/home.php
@@ -1,10 +1,81 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mobarok Hossen
- * Date: 2/10/2021
- * Time: 1:35 PM
- */
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>CI HMVC</title>
 
-echo "Hi";
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Welcome to Codeigniter HMVC  
+                </div>
+
+                <div class="links">
+                    <a href="https://github.com/mobarokhossen/codeigniter-hmvc">Documentation</a>
+                    <a href="https://github.com/mobarokhossen/codeigniter-hmvc">GitHub</a>
+                    <a href="https://mobarokhossen.com"> Contact Developer - Mobarok </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
